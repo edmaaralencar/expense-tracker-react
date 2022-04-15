af72594154f94897abae5e1d430566261784b8aa
Edmar Alencar <edmaaralencar1@gmail.com>
2022-04-15 12:50:54 -0300

    close sidebar when click link

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -4,27 +4,27 @@ import { FiHome, FiArrowUp, FiNavigation, FiUser } from 'react-icons/fi'
 import { useAppContext } from '../../context/AppContext'
 
 const Sidebar = () => {
-  const { showSidebar } = useAppContext()
+  const { showSidebar, toggleSidebar } = useAppContext()
 
   return (
     <S.Container className={showSidebar ? 'active' : ''}>
       <S.NavList>
-        <S.NavAnchor end={true} to="/">
+        <S.NavAnchor end={true} to="/" onClick={() => toggleSidebar()}>
           <FiHome size={35} color="#a7a8ad" />
           <span>Dashboard</span>
         </S.NavAnchor>
 
-        <S.NavAnchor to="/transactions">
+        <S.NavAnchor to="/transactions" onClick={() => toggleSidebar()}>
           <FiArrowUp size={35} color="#a7a8ad" />
           <span>Transações</span>
         </S.NavAnchor>
 
-        <S.NavAnchor to="/savings">
+        <S.NavAnchor to="/savings" onClick={() => toggleSidebar()}>
           <FiNavigation size={35} color="#a7a8ad" />
           <span>Poupança</span>
         </S.NavAnchor>
 
-        <S.NavAnchor to="/profile">
+        <S.NavAnchor to="/profile" onClick={() => toggleSidebar()}>
           <FiUser size={35} color="#a7a8ad" />
           <span>Minha Conta</span>
         </S.NavAnchor>
